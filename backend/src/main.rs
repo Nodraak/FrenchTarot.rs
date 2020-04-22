0858f61c897dda3641bc0ec53137f6fe08f97760
Adrien Chardon <adrienchardon@mailoo.org>
2020-04-22 19:40:35 +0200

    Setup Rocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,84 @@
-use tarot_lib;
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use std::collections::HashMap;
+//use std::sync::atomic::{AtomicUsize, Ordering};
+
+//use rocket::request::{self, Form, FromRequest, Request};
+//use rocket::Request;
+//use rocket::response::Redirect;
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
+//use serde::Serialize;
+
+//use tarot_lib;
+
+
+#[get("/")]
+fn index() -> Template {
+    let context = HashMap::<&str, &str>::new();
+    Template::render("index", &context)
+}
+
+mod user {
+    use std::collections::HashMap;
+    use rocket_contrib::templates::Template;
+
+    #[get("/")]
+    pub fn get() -> Template {
+        let context = HashMap::<&str, &str>::new();
+        Template::render("user/index", &context)
+    }
+
+    #[post("/")]
+    pub fn post() -> Template {
+        let context = HashMap::<&str, &str>::new();
+        Template::render("user/index", &context)
+    }
+}
+
+mod game {
+    use std::collections::HashMap;
+    use rocket_contrib::templates::Template;
+
+    #[get("/")]
+    pub fn index() -> Template {
+        let context = HashMap::<&str, &str>::new();
+        Template::render("game/index", &context)
+    }
+
+    #[get("/create")]
+    pub fn create_get() -> Template {
+        let context = HashMap::<&str, &str>::new();
+        Template::render("game/create", &context)
+    }
+
+    #[post("/create")]
+    pub fn create_post() -> Template {
+        let context = HashMap::<&str, &str>::new();
+        Template::render("game/create", &context)
+    }
+
+    #[get("/play/<id>")]
+    pub fn play(id: String) -> Template {
+        let context = HashMap::<&str, &str>::new();
+        Template::render("game/play", &context)
+    }
+}
+
 
 fn main() {
-    println!("Hello, world! backend");
     tarot_lib::main();
+    rocket::ignite()
+        // routes
+        .mount("/", routes![index])
+        .mount("/user", routes![user::get, user::post])
+        .mount("/game", routes![game::index, game::create_get, game::create_post, game::play])
+        // templates and static
+        .attach(Template::fairing())
+        .mount("/static", StaticFiles::from("./static"))
+        // launch!
+        .launch();
 }
